8262958bb6b806a6d23636d5beba568fc14f03e9
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-04-02 14:54:54 +0300

    POST Requests from Android studio

--- frontend/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ frontend/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -8,8 +8,13 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -63,15 +68,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class UploadTask extends AsyncTask<String, String, String> {
+
+        URL url;
+        HttpURLConnection http;
+        HttpURLConnection con;
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        public UploadTask(){
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String data = params[1]; //data to post
+            Log.i("Data",data);
+            OutputStream out = null;
+
+
+            try {
+
+                URL url = new URL(urlString);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                out = new BufferedOutputStream(con.getOutputStream());
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                out.close();
+                con.connect();
+                Log.i("status", "real success");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         String url = "http://192.168.53.37/intermidiate%20Currency%20Coverter/backend/APIs/rate_api.php";
-
         DownloadTask task = new DownloadTask();
         task.execute(url);
+
+        String postUrl = "http://192.168.53.37/intermidiate%20Currency%20Coverter/backend/APIs/db_api.php";
+        UploadTask task1 = new UploadTask();
+        String jsonInputString = "{\"amount\": 700, \"rate\": \"bank\", \"currency\": \"USD\"}";
+        task1.execute(postUrl, jsonInputString);
+        Log.i("status", "success");
+
 
     }
 }
