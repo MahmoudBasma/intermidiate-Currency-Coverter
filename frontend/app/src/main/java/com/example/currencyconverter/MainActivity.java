67b680ca1d7457307bbf847627600f76fdf0329c
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-04-02 13:16:24 +0300

    rate API at Android studio (successful attempt)

--- frontend/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ frontend/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    class DownloadTask extends AsyncTask<String, Void, String>{
+        protected String doInBackground(String... urls){
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                Log.i("Attempting", "Attempt1");
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                    Log.i("Result", result);
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+        protected void onPostExecute(String s){
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://192.168.53.37/intermidiate%20Currency%20Coverter/backend/APIs/rate_api.php";
+
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
+
     }
 }
