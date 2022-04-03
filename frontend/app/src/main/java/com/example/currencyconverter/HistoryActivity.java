d51fa1cb57437bc11c1fe43e038dee539c98a4de
Mohamad Kawas <mohamad.kawas@lau.edu>
2022-04-03 09:01:17 +0300

    return home method done

--- frontend/app/src/main/java/com/example/currencyconverter/HistoryActivity.java
+++ frontend/app/src/main/java/com/example/currencyconverter/HistoryActivity.java
@@ -2,13 +2,15 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class HistoryActivity extends AppCompatActivity {
-
+Intent intent;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -21,10 +23,10 @@ public class HistoryActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.help:
-                //goToHelp();
+                goToHelp();
                 return true;
-            case R.id.history:
-                //showHistory();
+            case R.id.home:
+                backHome();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -35,5 +37,16 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+    }
+    private void goToHelp() {
+        String url = "https://www.thelebaneseguide.com/lira-rate";
+        intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    private void backHome() {
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
