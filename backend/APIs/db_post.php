260eb690646c73e09fea971c64dee2f79631c50f
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-04-05 11:29:48 +0300

    Update db_post.php

--- backend/APIs/db_post.php
+++ backend/APIs/db_post.php
@@ -1,17 +1,22 @@
 <?php
 
 include("db_info.php");
+$json = file_get_contents('php://input');
+echo $json;
+$data = json_decode($json);
+echo $data;
 
-$amount = $_POST["amount"];
-$rate = $_POST["rate"];
-$currency = $_POST["currency"];
+
+$amount = $data->"amount";
+$rate = $data->"rate";
+$currency = $data->"currency";
 
 $query = $mysqli->prepare("INSERT INTO conversions (amount, rate, currency) VALUES (?, ?, ?)");
 $query->bind_param("iss", $amount, $rate, $currency);
 $query->execute();
 
 $response = [];
-$response["status"] = "Mabrouk!";
+$response["status"] = "Success";
 
 $json_response = json_encode($response);
 echo $json_response;
